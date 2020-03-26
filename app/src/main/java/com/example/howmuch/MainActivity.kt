64078f35686c57104f2c6725f2c6b44550535b73
a0104 <akffhaos95@naver.com>
2020-03-26 17:23:19 +0900

    first commit

---
+++ app/src/main/java/com/example/howmuch/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.howmuch
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_plus.setOnClickListener {
+            val plusIntent = Intent(this, PlusActivity::class.java)
+            startActivity(plusIntent)
+        }
+    }
+}
